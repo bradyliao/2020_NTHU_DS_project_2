78258197b079fa1a764a4fa0753f95d29878bfa0
bradyliao <bradyliao123@gmail.com>
2020-11-16 20:48:56 +0800

    Initial Commit

---
+++ DS_project_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DS_project_2
+//
+//  Created by Brady Liao on 11/16/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
