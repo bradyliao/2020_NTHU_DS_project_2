284ca9a8b83e2fc9e9855748298bfc344c4c9ae2
bradyliao <bradyliao123@gmail.com>
2020-11-22 18:14:15 +0800

    bfs

--- DS_project_2/main.cpp
+++ DS_project_2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <queue>
 
 using namespace std ;
 
@@ -36,19 +37,12 @@ int **fileInput(string fileName, int &rows, int &columns, int &battery, int &cha
          if (temp == '1')
          {
             floor[i][j] = 1 ;
-         
-         
-         
          }
          if (temp == 'R')
          {
             floor[i][j] = 3 ;
             chargerRow = i ;
             chargerColumn = j ;
-         
-         
-         
-         
          }
       }
       //consume new line character
@@ -69,24 +63,38 @@ public:
       this->row = row ;
       this->column = column ;
       this->status = status ;
+      
    }
    
 // private data of a block
 
    int row, column, status ;
-   
+   vector<Node*> adjacentNodes ;
+   vector<Node*> ups ;
+   vector<Node*> downs ;
+   vector<Node*> backs ;
+   int backSteps ;
 };
 
 
 
-/*
-void buildNodes(vector<vector<Node>> &floorNodes, int **floorOriginal, int rows, int columns)
+//build nodes
+void buildNodes(int **floorOriginal, int rows, int columns, int chargerRow, int chargerColumn, Node *root)
 {
-   for (int i = 0; i < rows; i++)
-      for (int j = 0; j < columns; j++)
-         floorNodes[i][j] = Node(i, j, floorOriginal[i][j]) ;
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
 }
-*/
+
 
 
 
@@ -98,26 +106,132 @@ int main(int argc, const char * argv[])
    int **floorOriginal ;
    
    
+   
    if (argc == 2)
       floorOriginal = fileInput(argv[1], rows, columns, battery, chargerRow, chargerColumn) ;
    else
       return 1 ;
    
+   
+   //array for mapping floor
+   int **floorMapping = new int *[rows] ;
+   for (int i = 0; i < rows; i++)
+      for (int j = 0; j < columns; j++)
+         floorMapping[i][j] = floorOriginal[i][j] ;
 
-
-   //buildNodes
-   vector<vector<Node>> floorNodes(rows);
+   
+   //array for floor nodes
+   Node ***floorNodes ;
+   
+   floorNodes = new Node**[rows] ;
    for (int i = 0; i < rows; i++)
    {
-      for (int j = 0; j < columns; j++)
+      floorNodes[i] = new Node*[columns] ;
+      for (int j = 0; j < 0; j++)
       {
-         Node temp(i, j, floorOriginal[i][j]) ;
-         floorNodes[i].push_back(temp) ;
+         if (floorOriginal[i][j] != 1)
+         {
+            Node temp = Node(i, j, floorOriginal[i][j]) ;
+            floorNodes[i][j] = &temp ;
+         }
       }
    }
    
    
-   // adjacentNodes / connectNodes (BFS)
+   // root
+   Node *root = floorNodes[chargerRow][chargerColumn] ;
+   root->backSteps = 0 ;
+   floorMapping[chargerRow][chargerColumn] = 4 ;
+
+
+   //map nodes
+   queue<Node*> expendQueue ;
+   expendQueue.push(root) ;
+   
+
+   Node *current, *next ;
+   while (!expendQueue.empty())
+   {
+      current = expendQueue.front() ;
+      
+      
+      if (current->column - 1 >= 0)
+      {
+         next = floorNodes[current->row][current->column - 1] ;
+
+         if (floorMapping[current->row][current->column - 1] == 0)
+         {
+            current->downs.push_back(next) ;
+            next->ups.push_back(current) ;
+            next->backs.push_back(current) ;
+            next->backSteps = current->backSteps + 1 ;
+            current->adjacentNodes.push_back(next) ;
+            next->adjacentNodes.push_back(current) ;
+            
+            // flag for done mapping
+            floorMapping[current->row][current->column - 1] = 4 ;
+            //push to queue for leter check for next node
+            expendQueue.push(next) ;
+         }
+         
+         if (floorMapping[current->row][current->column - 1] == 4)
+         {
+            current->downs.push_back(next) ;
+            next->ups.push_back(current) ;
+            
+            // should consider 2 ways
+            if (next->backSteps > current->backSteps + 1)
+            {
+               next->backSteps = current->backSteps + 1 ;
+               next->backs.clear() ;
+               next->backs.push_back(current) ;
+            }
+            
+            if (next->backSteps == current->backSteps + 1)
+            {
+               next->backs.push_back(current) ;
+            }
+            
+            if (next->backSteps < current->backSteps + 1)
+            {
+               //do nothing
+            }
+            
+            current->adjacentNodes.push_back(next) ;
+            next->adjacentNodes.push_back(current) ;
+            
+            // flag for done mapping
+            floorMapping[current->row][current->column - 1] = 4 ;
+         }
+         
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      //done checking around this node, pop out of the queue
+      expendQueue.pop() ;
+   }
+   
+   
+   
+
+   for (int i = 0; i < rows; i++)
+   {
+      for (int j = 0; j < 0; j++)
+      {
+         cout << floorNodes[i][j]->status << " " ;
+      }
+      cout << '\n' ;
+   }
    
    
    
@@ -127,6 +241,19 @@ int main(int argc, const char * argv[])
 
 
 
+
+
+
+
+
+
+
+ 
+
+ 
+ 
+ 
+ 
 
 
 
